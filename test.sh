5490163c3d6584f116594cbb9f31b05419ab2f78
tbc <56662914+Tbc-tang@users.noreply.github.com>
2021-04-25 00:07:38 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,4 @@ for file in ./*.sh;do
     fi
     printf "==================== %s ====================\n" "$file"
     bash "$file"
-    printf "-------------------- %s --------------------\n" "$file"
-    shellcheck "$file"
 done
