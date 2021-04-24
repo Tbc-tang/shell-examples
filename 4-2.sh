c79d9eba163c79d04a0a6b9b8646e6f45c58ab7e
Tbc-tang <3168526759@qq.com>
2021-04-24 14:21:13 +0000

    demo

---
+++ 4-2.sh
@@ -0,0 +1,8 @@
+MyFirstLetters=ABC
+echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"
+
+greeting='Hello        world!'
+#双引号包围变量名可以保留所有空格字符
+
+# 此处实际上实现了一个字符串 拼接/连接 操作
+echo $greeting" now with spaces: $greeting"
---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+function adder {
+    arg_1=$1
+    arg_2=$2
+    echo $((arg_1 + arg_2))
+}
+
+adder 1 1
